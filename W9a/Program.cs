c85d50e8e733eb9807b6feb551e4fedcded6c276
NeoXypher <Aquiliawtf@gmail.com>
2018-11-27 09:27:07 +0000

    1st

---
+++ W9a/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace W9a
+{
+	class Program
+	{
+		
+		static void Mai2(string[] args)
+		{
+			int Size1 = 3, Size2 = 3;
+			int[] Array1 = new int[] { 1, 2, 3 };
+			int[] Array2 = new int[] { 1, 2, 3 };
+			int[] Array3 = new int[] { };
+			for (int i = 0; i < Size1; i++)
+			{
+				Array3[i] = Array1[i];
+				Array3[i + Size1] = Array2[i];
+			}
+			
+
+		}
+	}
+}
---
+++ W9a/W09aT4.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace W9a
+{
+	class W09aT4
+	{
+		
+		public static void Main()
+		{
+			int[] Scores = new int[30];
+			int Total = 0, Average = 0;
+			Random rnd = new Random();
+			for (int i = 0; i < 30; i++)
+			{
+				Scores[i] = rnd.Next(1, 101);
+				Total += Scores[i];
+			}
+			Average = Total / 30;
+			Console.WriteLine(Average);
+			Console.ReadLine();
+		}
+
+		public void Add()
+		{
+			int[] Array1 = new int[] { 1, 2, 3 };
+			int[] Array2 = new int[] { 1, 2, 3 };
+			int[] Array3 = new int[Array1.Length];
+			if (Array1.Length == Array2.Length)
+			{
+				for (int i = 0; i < Array1.Length; i++)
+				{
+					Array3[i] = Array1[i] + Array2[i];
+				}
+			}
+		}
+	}
+}
